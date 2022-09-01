2fdd16a56aae4f1d16069c9263864cff4c8ee552
Gustavo Poletto <gustavo.poletto@g.globo>
2022-09-01 15:36:36 -0300

    chore:project structure

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss'
+
+function Header() {
+  return (
+    <div className={styles['header']}>
+      <h2>Game Room</h2>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/rooms/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.module.scss'
+
+function Rooms() {
+  return (
+    <section className={styles['rooms']}>
+      
+    </section>
+  );
+}
+
+export default Rooms;
